5df5b5c82ed4833197800ee4ef136d81504921a2
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-20 11:26:21 +0300

    user router use

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-require("./db")
+require("./db");
+
+// App
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Routers
+const userRouter = require("./routers/route/user");
+app.use(userRouter);
+
+// PORT
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON ${PORT}`);
