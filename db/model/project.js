b3b1d0f0e0d800f7df194dc0a9b9ce78266a3dd0
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-20 11:06:59 +0300

    schema project

---
+++ db/model/project.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const project = new mongoose.Schema({
+  comment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  package: { type: mongoose.Schema.Types.ObjectId, ref: "Package" },
+  describe: { type: String, required: true },
+  title: { type: String, required: true },
+  kind: { type: String, required: true },
+  deadline: { type: String, required: true },
+  goal: { type: Number, required: true },
+  img: { type: String, required: true },
+  desc: { type: String, required: true },
+  time: { type: Date },
+  location: { type: String },
+  pledged: { type: Number },
+  isDel: { type: Boolean, default: false },
+  img1: { type: String },
+  img2: { type: String },
+  img3: { type: String },
+  img4: { type: String },
+  img5: { type: String },
+  img6: { type: String },
+  desc1: { type: String },
+  desc2: { type: String },
+  desc3: { type: String },
+  desc4: { type: String },
+  desc5: { type: String },
+  desc6: { type: String },
+});
+
+module.exports = mongoose.model("Project", project);
