31f5c15366bb14c7fa43e26000bb61d3e6bf34ef
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-20 11:23:08 +0300

    register function

---
+++ routers/controller/user.js
@@ -0,0 +1,26 @@
+const userModel = require("../../db/model/user");
+
+const bcrypt = require("bcrypt");
+
+const register = (req, res) => {
+  const { email, name, username, password } = req.body;
+
+  try {
+    const newUser = userModel({
+      email,
+      name,
+      username,
+      password,
+    })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { register };
