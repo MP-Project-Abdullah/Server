129c3a107d2ada566dd65f921a1e9a1934c8a728
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2022-01-02 17:02:08 +0300

    router reject and approve story

--- routers/route/story.js
+++ routers/route/story.js
@@ -2,13 +2,28 @@ const express = require("express");
 const storyRouter = express.Router();
 const authentication = require("../middlewear/authentication");
 
-const { newStory, getStories, getStory } = require("../controller/story");
+const {
+  newStory,
+  getStories,
+  getStory,
+  getStoriesNotApproved,
+  approvedStory,
+  rejectStory,
+} = require("../controller/story");
 
 // Create new story
 storyRouter.post("/newStory/:_id", newStory);
 
+storyRouter.put("/aproovedStory/:_id", approvedStory);
+
 // Get all stories
 storyRouter.get("/stories", getStories);
+
+// Approved story
+storyRouter.get("/storiesNotApproved", getStoriesNotApproved);
+
+// Reject story
+storyRouter.put("/rejectStory/:_id", rejectStory);
 
 // Get one story
 storyRouter.get("/story/:_id", getStory);
